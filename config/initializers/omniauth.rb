a6b566733a848a50accfb66638555cd1659c45ac
Scott Mazza <scott@scottmazza.com>
2012-11-01 19:18:31 -0400

    Add Facebook app for test system

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,7 +3,13 @@ OmniAuth.config.logger = Rails.logger
 Rails.application.config.middleware.use OmniAuth::Builder do
   # Facebook authentication info  
   # Need to remove these from source control
-  ENV['FACEBOOK_APP_ID'] = "463074820377113"
-  ENV['FACEBOOK_SECRET'] = "3fef8d4dbf6e11ef205912df59dcd38a"
+  if Rails.env.production?
+    ENV['FACEBOOK_APP_ID'] = "463074820377113"
+    ENV['FACEBOOK_SECRET'] = "3fef8d4dbf6e11ef205912df59dcd38a"
+  else
+    ENV['FACEBOOK_APP_ID'] = "551122088246583"
+    ENV['FACEBOOK_SECRET'] = "61265f659177cca692a6712cea0473ca"
+  end
+    
   provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_SECRET']
 end
