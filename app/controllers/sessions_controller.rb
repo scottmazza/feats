941d18f403b6eb3eed450d50d273e980f04f0e19
Scott Mazza <scott@scottmazza.com>
2012-11-02 11:58:54 -0400

    Automatic account creation. No signup page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,39 +3,33 @@ class SessionsController < ApplicationController
   end
   
   def create
-    # ----------------------------------------------------------------------- #
-    # Facebook redirects here after successful login. We'll first make sure   #
-    # that the email address they are signed into FB with is in our database. #
-    # ----------------------------------------------------------------------- #
-    
+    # 
+    # Facebook redirects here after successful login. We'll first ensure that
+    # there is a corresponding User record in our DB.
+    #    
     auth = request.env["omniauth.auth"]
-    user = User.find_by_email(auth.info.email)
-    if !user
-      flash[:error] = "Please sign up first." 
-      redirect_to new_user_path
-    else
-      # --------------------------------------------------------------------- #
-      # The user is in our DB. We'll record the FB auth info in our DB,       #
-      # create a session, and redirect the user to our root page.             #
-      # --------------------------------------------------------------------- #
-      
-      user.update_attributes!(
-        oauth_provider:   auth.provider,
-        oauth_token:      auth[:credentials][:token],
-        oauth_expires_at: Time.at(auth.credentials.expires_at),   
-        name:             auth.info.name,
-        image:            auth.info.image )
-   
-      session[:user_id] = user.id
-      if user.profile_complete?
-        if user.has_feats?
-          redirect_to root_url
-        else
-          redirect_to search_feats_path
-        end
+    user = User.where( email: auth.info.email ).first_or_create!
+    # --------------------------------------------------------------------- #
+    # The user is in our DB. We'll record the FB auth info in our DB,       #
+    # create a session, and redirect the user to our root page.             #
+    # --------------------------------------------------------------------- #
+    
+    user.update_attributes!(
+      oauth_provider:   auth.provider,
+      oauth_token:      auth[:credentials][:token],
+      oauth_expires_at: Time.at(auth.credentials.expires_at),   
+      name:             auth.info.name,
+      image:            auth.info.image )
+ 
+    session[:user_id] = user.id
+    if user.profile_complete?
+      if user.has_feats?
+        redirect_to root_url
       else
-        redirect_to controller: 'users', action: 'edit', id: user.id
+        redirect_to search_feats_path
       end
+    else
+      redirect_to controller: 'users', action: 'edit', id: user.id
     end
   end
   
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,24 +1,10 @@
 class UsersController < ApplicationController
   before_filter :signed_in_user, only: [:edit, :update]
   
-  def create
-    @user = User.new(params[:user])
-    if @user.save
-      flash[:info] = "Signup successful."
-      redirect_to '/signin'
-    else
-      render 'new'
-    end
-  end
-  
   def edit
     @user = User.find(params[:id])
   end
-  
-  def new
-    @user = User.new
-  end
-  
+    
   def show
     @user = User.find(params[:id])
     @feat_count = Feat.count_by_user_id( params[:id] )
